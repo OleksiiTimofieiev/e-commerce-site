237a6218614f9d241f149290efae054e14439c30
contrbas42 <contrbas@gmail.com>
2018-04-07 12:01:11 +0300

    c

--- ecommerce/admin_area/insert_cat.php
+++ ecommerce/admin_area/insert_cat.php
@@ -1,14 +1,25 @@
 <form action ="" method="post" style="padding: 40px">
 	<b>Insert New Category:</b>
 	<input type="text" name="new_cat" >
-	<input type="submit" name="add_cat" value="Add category">
+	<input type="submit" name="add_cat" value="Add category" required>
 </form>
 
 <?php
-	include("includes/db.php")
+	include("includes/db.php");
 
-	$new_cat = $_POST['new_cat'];
+	if(isset($_POST['add_cat']))
+	{
+		$new_cat = $_POST['new_cat'];
 
-	$insert_
+		$insert_cat = "insert into categories (cat_title) values ('$new_cat')";
+
+		$run_cat = mysqli_query($con, $insert_cat);
+
+		if($run_cat)
+		{
+			echo "<script>alert('New category has been inserted.')</script>";
+			echo "<script>window.open('index.php?view_cats')</script>";
+		}
+	}
 
 ?>
